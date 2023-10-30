b96988dd98179e140dd6a0a5afce33c0c72615d7
atile <144712135+atile4@users.noreply.github.com>
2023-10-30 11:50:44 -0700

    wrote a better README

--- AttClass.java
+++ AttClass.java
@@ -1,23 +1,4 @@
 public class AttClass {
-/*
-AttClass a = new AttClass();
-
-validCheck - Checks if a String/char input is valid.
-validCheck(String, String[]) -> boolean
-
-Ex:
-char[] inputs = {'a', 'b', 'c', 'd'};
-System.out.println(a.validCheck('a', inputs));  -> returns true
-System.out.println(a.validCheck('j', inputs));  -> returns false
-
-String[] inputs = {"red", "blue", "green", "yellow"};
-System.out.println(a.validCheck("blue", inputs));  -> returns true
-System.out.println(a.validCheck("purple", inputs));  -> returns false
-
-array input MUST be initialized first.
- */
-
-
     public boolean validCheck(String input, String[] l) {
         for (String s : l) {
             if (input.equals(s)) {
